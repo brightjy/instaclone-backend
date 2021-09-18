f5d95fc54e30509a55cc48054c48517b7f89d9a5
박지영 <barkji0@naver.com>
2021-09-19 00:21:45 +0900

    basic settings

--- schema.js
+++ schema.js
@@ -1,9 +1,9 @@
-import { 
-  loadFilesSync
-  , makeExecutableSchema
-  , mergeResolvers
-  , mergeTypeDefs 
-} from "@graphql-tools/utils";
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import { loadFilesSync } from '@graphql-tools/load-files';
+import { mergeResolvers, mergeTypeDefs } from '@graphql-tools/merge';
+import path from 'path';
+
+const __dirname = path.resolve();
 
 const loadedTypes =  loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
 const loadedResolvers = loadFilesSync(`${__dirname}/**/*.{queries,mutations}.js`);
@@ -11,6 +11,6 @@ const loadedResolvers = loadFilesSync(`${__dirname}/**/*.{queries,mutations}.js`
 const typeDefs = mergeTypeDefs(loadedTypes);
 const resolvers = mergeResolvers(loadedResolvers);
 
-const schema = makeExecutableSchema({typeDefs, resolvers});
+const schema = makeExecutableSchema({typeDefs,resolvers});
 
 export default schema;
