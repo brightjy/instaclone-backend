5337946efc188b4fdc39cbd8fe800e371c472424
박지영 <barkji0@naver.com>
2021-09-19 20:48:42 +0900

    create prisma DB about Users

---
+++ users/users.mutations.js
@@ -0,0 +1,27 @@
+import client from "../client";
+
+export default {
+  Mutation: {
+    createAccount: async (
+      _, 
+      { firstName, lastName, userName, email, password}
+      ) => {
+      // check if username or email are already on DB.
+      const existingUser = await client.user.findFirst({
+        where: {
+          OR: [
+            {
+              userName: userName,
+            },
+            {
+              email,
+            },
+          ],
+        },
+      });
+      console.log(existingUser);
+      // hash password
+      // save and return the user
+    },
+  },
+};
