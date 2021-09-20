6f12c27429f7c9044b612517ac1aeae71fdb7103
박지영 <barkji0@naver.com>
2021-09-20 17:40:17 +0900

    make editProfile, login, seeProfile and change the architect of folder

---
+++ users/seeProfile/seeProfile.resolvers.js
@@ -0,0 +1,12 @@
+import client from "../../client";
+
+export default {
+  Query: {
+    // findUnique is only for @unique field.
+    seeProfile: (_, { userName }) => client.user.findUnique({
+      where: {
+        userName,
+      },
+    }),
+  },
+};
