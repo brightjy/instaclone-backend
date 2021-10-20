03cc45347e295eff2e621d58a3f253ca5eeb2870
박지영 <barkji0@naver.com>
2021-10-21 00:31:52 +0900

    computed field

--- users/seeFollowing/seeFollowing.resolvers.js
+++ users/seeFollowing/seeFollowing.resolvers.js
@@ -2,6 +2,7 @@ import client from "../../client"
 
 export default {
   Query: {
+    // cursor based pagination 은 무제한 스크롤에 유용하다. (단점: 특정 페이지로의 이동은 어려움)
     seeFollowing: async(_, {userName, lastId}) => {
       const ok = await client.user.findUnique({
         where: {userName},
