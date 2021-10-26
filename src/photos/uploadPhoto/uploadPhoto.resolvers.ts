f52bc7dd92383233eb64564c5a20d29a3443e1c2
박지영 <barkji0@naver.com>
2021-10-26 23:33:52 +0900

    edit photo

--- src/photos/uploadPhoto/uploadPhoto.resolvers.ts
+++ src/photos/uploadPhoto/uploadPhoto.resolvers.ts
@@ -1,5 +1,6 @@
 import client from "../../client";
 import { protectResolver } from "../../users/users.utils";
+import { processHashtags } from "../photos.utils";
 
 export default {
   Mutation: {
@@ -7,14 +8,14 @@ export default {
       async(_, {file, caption}, {loggedInUser}) => {
         let hashtagObjs = [];
         if(caption) {
+          hashtagObjs = processHashtags(caption)
           // parse caption
-          const hashtags = caption.match(/#[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\w]+/g);
+          // const hashtags = caption.match(/#[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\w]+/g);
           // get or create Hashtags 
-          hashtagObjs = hashtags.map(hashtag => ({
-            where: {hashtag}, 
-            create: {hashtag},
-          }));
-          console.log(hashtagObjs);
+          // hashtagObjs = hashtags.map(hashtag => ({
+          //   where: {hashtag}, 
+          //   create: {hashtag},
+          // }));
         }
         return client.photo.create({
           data: {
